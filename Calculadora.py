6f4b3e6a3060f53f5e531c613b3158d62d9442ff
ARMENTA MARTINEZ ROBERTO RAZIEL <rraziel14@gmail.com>
2025-09-18 09:08:18 -0600

    Create Calculadora.py
    Primera funcion de suma

---
+++ Calculadora.py
@@ -0,0 +1,4 @@
+print("Funcion de suma de dos numeros")
+n1 = float(input("Ingresa el Primer numero: "))
+n2 = float(input("Ingresa el Segundo numero: "))
+print("La suma de los dos numeros es: ", n1 + n2)
